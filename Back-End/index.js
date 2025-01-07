c0e8744271660a9ceb0b5829ff009fcef43f61ea
Thusikaran <pushparasa.thusikaran@gmail.com>
2025-01-07 15:08:42 +0530

    backend is finish

--- Back-End/index.js
+++ Back-End/index.js
@@ -3,6 +3,9 @@ var cors = require('cors');
 const connection = require('./connection');
 const userRoute = require('./routes/user');
 const categoryRoute = require('./routes/category');
+const productRoute = require('./routes/product');
+const billRoute = require('./routes/bill');
+const dashboardRoute = require('./routes/dashboard');
 const app = express();
 
 app.use(cors());
@@ -10,5 +13,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/user',userRoute);
 app.use('/category',categoryRoute);
+app.use('/product',productRoute);
+app.use('/bill',billRoute);
+app.use('/dashboard',dashboardRoute);
 
 module.exports =app;
